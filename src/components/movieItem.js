67163050ace5c5f6d11fe0d515ca9cd2bd850090
mikecahillmike <48316633+mikecahillmike@users.noreply.github.com>
2020-11-01 23:45:35 +0000

    lab

--- src/components/movieItem.js
+++ src/components/movieItem.js
@@ -6,10 +6,6 @@ export class MovieItem extends React.Component {
     render() {
         return (
             <div>
-                <h4>{this.props.movie.Title}</h4>
-                <p>{this.props.movie.Year}</p>
-                <img src={this.props.movie.Poster} width="200" height="200" ></img>
-
                 <Card>
                     <Card.Header>{this.props.movie.Title}</Card.Header>
                     <Card.Body>
